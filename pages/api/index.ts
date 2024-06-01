3d1057b639fdc0e9dcb38855276131c5268b274f
Roberto Nicoletti <roberto.urias@gmail.com>
2024-06-01 16:32:23 -0300

    atualizando explicações das aulas

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,10 +1,10 @@
-/* eslint-disable no-console */
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
+  // eslint-disable-next-line no-console
   console.log(request.headers);
   response.status(200).json({ message: "Retorno da API" });
 }
