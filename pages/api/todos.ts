dfc675bfe58a909d63c124351cc8ad9dbce86ccb
Roberto Nicoletti <roberto.urias@gmail.com>
2024-06-14 10:52:46 -0300

    Create Todo

--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -9,6 +9,11 @@ export default function handler(
     todoController.get(request, response);
     return
   }
+  
+  if (request.method === "POST") {
+    todoController.create(request, response);
+    return
+  }
 
   return response.status(405).json({ message: "Method not allowed" });
 }
--- src/server/controller/todo.ts
+++ src/server/controller/todo.ts
@@ -1,7 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { z as schema } from "zod"
+
 import { todoRepository } from "@server/repository/todo";
 
-function get(req: NextApiRequest, res: NextApiResponse) {
+async function get(req: NextApiRequest, res: NextApiResponse) {
   const query = req.query;
   const page = Number(query.page);
   const limit = Number(query.limit);
@@ -35,6 +37,32 @@ function get(req: NextApiRequest, res: NextApiResponse) {
   });
 }
 
+const TodoCreateBodySchema = schema.object({
+  content: schema.string(),
+})
+async function create(req: NextApiRequest, res: NextApiResponse) {
+  const body = TodoCreateBodySchema.safeParse(req.body)
+
+  // Type Narrowing
+  if(!body.success) {
+    res.status(400).json({
+      error: {
+        message: "You need to provide a content to create a TODO",
+        description: body.error.issues
+      }
+    })
+    return
+  }
+
+  // Here we have to data
+  const createdTodo = await todoRepository.createByContent(body.data.content)
+  
+  res.status(201).json({
+    todo: createdTodo,
+  })
+}
+
 export const todoController = {
   get,
+  create,
 };
